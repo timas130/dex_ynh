5c4b4746fafe21c65bdd1ab60fab349cfb760ab2
sit <me@sit.sh>
2025-07-09 20:09:07 +0300

    change docker_image to my own

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,13 +6,15 @@
 # borrowed here: https://github.com/YunoHost-Apps/vaultwarden_ynh/blob/master/scripts/_common.sh
 
 _download_dex_from_docker() {
-    docker_image="dexidp/dex"
-    debian=$(lsb_release --codename --short)
-    if [[ $debian = "bullseye" ]]; then
-        docker_version="$(ynh_app_upstream_version)-distroless"
-    elif [[ $debian = "bookworm" ]]; then
-        docker_version="$(ynh_app_upstream_version)-alpine"
-    fi
+    #docker_image="dexidp/dex"
+    docket_image="ghcr.io/timas130/dex"
+    #debian=$(lsb_release --codename --short)
+    #if [[ $debian = "bullseye" ]]; then
+    #    docker_version="$(ynh_app_upstream_version)-distroless"
+    #elif [[ $debian = "bookworm" ]]; then
+    #    docker_version="$(ynh_app_upstream_version)-alpine"
+    #fi
+    docker_version="latest"
 
     docker_arg=""
     # Fixup for armhf
